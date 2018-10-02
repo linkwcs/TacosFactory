31db9c6577a8d33bd7f37363ef0bf0399eeab7d2
Bastien Krettly <bastien_krettly@wilder.school>
2018-10-02 15:13:06 +0200

    init project

---
+++ app/src/main/java/fr/wildcodeschool/tacosfactory/TacosActivity.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.tacosfactory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TacosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
